e822acfee6e50e94ad9a3045e4aad77a65665c3f
Gunnar Schwerin <wschweri1@ltu.edu>
2024-10-12 19:24:38 -0400

    created initial setup for tables

---
+++ FantasyWebApp/FantasyWebApp/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace FantasyWebApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FantasyWebApp.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FantasyWebApp.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
